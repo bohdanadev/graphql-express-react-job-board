32a5fc7d47bdaf3cf38a456e7fd1f2e9aed60b64
Bohdana K <bohdana.dev@gmail.com>
2024-07-05 16:45:30 +0300

    Add queries

---
+++ client/src/lib/graphql/queries.js
@@ -0,0 +1,61 @@
+import { GraphQLClient, gql } from "graphql-request";
+import { GRAPHQL_URL } from "../../keys.js";
+
+console.log("GQL:", GRAPHQL_URL);
+const client = new GraphQLClient(GRAPHQL_URL);
+
+export async function getCompany(id) {
+  const query = gql`
+    query CompanyById($id: ID!) {
+      company(id: $id) {
+        id
+        name
+        description
+        jobs {
+          id
+          date
+          title
+        }
+      }
+    }
+  `;
+  const { company } = await client.request(query, { id });
+  return company;
+}
+
+export async function getJob(id) {
+  const query = gql`
+    query JobById($id: ID!) {
+      job(id: $id) {
+        id
+        date
+        title
+        company {
+          id
+          name
+        }
+        description
+      }
+    }
+  `;
+  const { job } = await client.request(query, { id });
+  return job;
+}
+
+export async function getJobs() {
+  const query = gql`
+    query {
+      jobs {
+        id
+        date
+        title
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+  const { jobs } = await client.request(query);
+  return jobs;
+}
---
+++ server/resolvers.js
@@ -0,0 +1,20 @@
+import { getCompany } from "./db/companies.js";
+import { getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
+
+export const resolvers = {
+  Query: {
+    company: (_root, { id }) => getCompany(id),
+    job: (_root, { id }) => getJob(id),
+    jobs: () => getJobs(),
+  },
+  Job: {
+    company: (job) => getCompany(job.companyId),
+    date: (job) => toIsoDate(job.createdAt),
+  },
+  Company: {
+    jobs: (company) => getJobsByCompany(company.id),
+  },
+};
+function toIsoDate(value) {
+  return value.slice(0, "yyyy-mm-dd".length);
+}
